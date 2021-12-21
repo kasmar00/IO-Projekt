5a38411f38dca73bd10ef9d0642d69d16143f65f
Marcin Kasznia <kasmar00@gmail.com>
2021-12-21 20:04:16 +0100

    #10 | add documentation to transform method

--- src/main/java/pl/put/poznan/transformer/logic/text/Text.java
+++ src/main/java/pl/put/poznan/transformer/logic/text/Text.java
@@ -3,6 +3,12 @@ package pl.put.poznan.transformer.logic.text;
 /**
  * Text interface which handles transform method
  */
-public interface Text  {
+public interface Text {
+
+    /**
+     * Applying transformation to object
+     *
+     * @return string with applied transformation
+     */
     String transform();
 }
