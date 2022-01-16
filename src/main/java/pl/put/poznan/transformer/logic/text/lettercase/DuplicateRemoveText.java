80ad2b612ed89882f8b82518e09e9e4dfd3a17ee
AnnaPralat <anna@pralat.pl>
2022-01-16 12:57:57 +0100

    #8 add remove duplicate transformation

---
+++ src/main/java/pl/put/poznan/transformer/logic/text/lettercase/DuplicateRemoveText.java
@@ -0,0 +1,58 @@
+package pl.put.poznan.transformer.logic.text.lettercase;
+
+import pl.put.poznan.transformer.logic.exceptions.DomainException;
+import pl.put.poznan.transformer.logic.text.Text;
+import pl.put.poznan.transformer.logic.text.TextTransformer;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * Remove duplicate text
+ * <p>
+ * Removes duplicates from the given text
+ */
+public class DuplicateRemoveText extends TextTransformer {
+    /**
+     * Duplicate remove text class constructor
+     * <p>
+     * Refers to inherited variable text from parent's class
+     *
+     * @param text Text to be transformed
+     */
+    public DuplicateRemoveText(Text text) {super(text);}
+
+    /**
+     * Keyword for remove duplicates text transformation
+     */
+    public static String name = "duplicate";
+
+    /**
+     * Removes duplicates from the given text
+     *
+     * @return Text with removed duplicates
+     */
+    @Override
+    public String transform() throws DomainException {
+        return setRemoveDuplicate(text.transform());
+    }
+
+    /**
+     * Transforms text removing duplicates from the given text
+     *
+     * @param text Holds the text
+     * @return Text without duplicates
+     */
+    private String setRemoveDuplicate(String text) {
+        String[] words = text.split(" ");
+
+        ArrayList<String> result = new ArrayList<>();
+        result.add(words[0]);
+        for (int i = 1; i < words.length; ++i) {
+            if (!Objects.equals(result.get(result.size() - 1), words[i])) {
+                result.add(words[i]);
+            }
+        }
+        return String.join(" ", result);
+    }
+}
